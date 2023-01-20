ae411026e89edd1c737ad70083c395ccf3cfa2c0
unknown <pedro.vitor@mail.com>
2023-01-20 15:30:54 -0300

    fist commit

---
+++ mircoservice/src/main/java/microservice/mircoservice/MircoserviceApplication.java
@@ -0,0 +1,13 @@
+package microservice.mircoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MircoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MircoserviceApplication.class, args);
+	}
+
+}
